0f536a6e072ed3e24610385ee2619b68905ef982
pelevin-d-y <natpuot1992@gmail.com>
2018-10-03 18:23:50 +0300

    add fonts roboto

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,23 +1,25 @@
-var dropDownMenu = function(elements, buttonElementClass, dropContentClass, contentHeightClass) {
-  elements.each(function(i, element) {
-    const buttonElement = element.querySelector('.' + buttonElementClass)
-    const dropContent = element.querySelector('.' + dropContentClass)
-    const contentHeight = dropContent.querySelector('.' + contentHeightClass).offsetHeight
-  
-    buttonElement.addEventListener('click', function() {
-      if (element.classList.contains('active')) {
-        element.classList.remove('active')
-        dropContent.style.height = 0 + 'px'
-      } else {
-        element.classList.add('active')
-        dropContent.style.height = contentHeight + 'px'
-      }
-    })
+var addDropDownMenuListener = function(element, buttonElementClass, dropContentClass, contentHeightClass) {
+  const buttonElement = element.querySelector('.' + buttonElementClass)
+  const dropContent = element.querySelector('.' + dropContentClass)
+  const contentHeight = dropContent.querySelector('.' + contentHeightClass).offsetHeight
+
+  buttonElement.addEventListener('click', function() {
+    if (element.classList.contains('active')) {
+      element.classList.remove('active')
+      dropContent.style.height = 0 + 'px'
+    } else {
+      element.classList.add('active')
+      dropContent.style.height = contentHeight + 'px'
+    }
   })
 }
 
 var prices = $('.price-page__item')
-dropDownMenu(prices, 'price-page__item-title', 'price-page__item-content', 'item-page__price')
+prices.each(function(i, element) {
+  addDropDownMenuListener(element, 'price-page__item-title', 'price-page__item-content', 'item-page__price')
+})
 
 var contacts = $(document.querySelectorAll('.contacts-page__faces-item-dd'))
-dropDownMenu(contacts, 'contacts-page__faces-item-dd-title', 'contacts-page__faces-item-dd-wrapper', 'contacts-page__faces-item-wrapper')
+contacts.each(function(i, element) {
+  addDropDownMenuListener(element, 'contacts-page__faces-item-dd-title', 'contacts-page__faces-item-dd-wrapper', 'contacts-page__faces-item-wrapper')
+})
